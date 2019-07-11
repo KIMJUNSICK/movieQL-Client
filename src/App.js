225f3e72ba0c68734c3105af705ba710e59f5ddf
Kim Jun Sik <jinaganda5@gmail.com>
2019-07-11 09:20:35 +0900

    Setting up an Apollo Client

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { Component } from "react";
+import client from "./client";
+import { ApolloProvider } from "react-apollo";
 
-function App() {
-  return <div className="App" />;
+class App extends Component {
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <div className="App">hello, junsik. This is my Apollo world!</div>
+      </ApolloProvider>
+    );
+  }
 }
 
 export default App;
